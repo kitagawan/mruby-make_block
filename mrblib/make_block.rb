8d2d7ddaee7b459f58425b7ac0fa6292302d8816
kitagawan <tetsuya@sein.mse.kyutech.ac.jp>
2019-11-12 15:29:31 +0900

    edit mrblib

--- mrblib/make_block.rb
+++ mrblib/make_block.rb
@@ -9,18 +9,6 @@ class Make_block
         @m_count=0
         @address=Generate_Address.new
     end
-
-    def mini_create
-        time=Time.now
-        time=time.to_s
-        str=time.to_s.split(//)
-        #Bignumに変換
-        data=str[11].to_i+str[12].to_i+str[17].to_i+str[18].to_i
-        #Fixnumに変換
-        data=data.to_fix
-        @mini_block[@m_count]=Mini_Block.new(time,data)
-        @m_count=@m_count+1
-    end   
     
     def connect
         @address.genekey2
