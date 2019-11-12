4caa1b805d4b7780a91d9d43759e14f607d9c210
kitagawan <tetsuya@sein.mse.kyutech.ac.jp>
2019-11-12 15:30:41 +0900

    edit mrblib

--- mrblib/make_block.rb
+++ mrblib/make_block.rb
@@ -1,12 +1,3 @@
 class Make_block
-    def initialize
-        @new_hash="kdmehfb43ijsqwioxz49gn28ewplonfa"
-        Mini_Block= Struct.new(:time,:data)
-        Block= Struct.new(:prev_hash,:mini_block)
-        @block=Array.new()
-        @mini_block=Array.new()
-        @count=0
-        @m_count=0
-        @address=Generate_Address.new
-    end
+
 end
